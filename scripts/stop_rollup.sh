7210acc67773a450bb58ecfad469c0a7e1f677ee
0xtrou <khangtran.btc@gmail.com>
2023-10-30 18:09:15 +0700

    chore: updated operation scripts

--- scripts/stop_rollup.sh
+++ scripts/stop_rollup.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-
-# Stop the batcher
-curl -d '{"id":0,"jsonrpc":"2.0","method":"admin_stopBatcher","params":[]}' \
-    -H "Content-Type: application/json" http://localhost:8548 | jq
-
 # Stop the node and geth
+docker compose stop opbatcher
 docker compose stop opnode
 docker compose stop opgeth
 
